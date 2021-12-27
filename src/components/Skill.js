7e3aba33535a20654bc0b22aa96cd7bccc0e8d44
Peter Abah <peterabah.ob@gmail.com>
2021-12-27 10:24:38 +0100

    Add styles for skill component

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,9 +1,9 @@
 const Skill = (props) => {
   const { name, id, deleteSkill } = props;
   return (
-    <div>
-      <p>{name}</p>
-      <button data-id={id} onClick={deleteSkill}>Delete</button>
+    <div className="skill">
+      <p className="skill__name">{name}</p>
+      <button className="form__btn form__btn--small" data-id={id} onClick={deleteSkill}>Delete</button>
     </div>
   );
 };

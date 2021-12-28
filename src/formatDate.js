3a4f0cbde3e8f66997a127ac2bb2d41ce7bc5b62
Peter Abah <peterabah.ob@gmail.com>
2021-12-28 17:14:40 +0100

    Fix bug in formatDate function

--- src/formatDate.js
+++ src/formatDate.js
@@ -2,7 +2,7 @@ import { format } from 'date-fns';
 
 const formatDate = (datestr) => {
   const date = new Date(datestr);
-  return format(date, 'MMMM d');
+  return format(date, 'MMMM yyyy');
 };
 
 export default formatDate;

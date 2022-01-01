d2e81e43b4b61837bc7e95056fbe2438d6bc94d4
Peter Abah <peterabah.ob@gmail.com>
2022-01-01 18:06:57 +0100

    Fix some styling issues

--- src/components/EducationPreview.js
+++ src/components/EducationPreview.js
@@ -5,11 +5,9 @@ const EducationPreview = (props) => {
 
   return (
     <div className="preview__info">
-      <div>
-        <h4 className="preview__info__title">{degree} {course}</h4>
-        <p>{school}</p>
-        <p className="preview__info__date">{formatDate(graduationYear)}</p>
-      </div>
+      <h4 className="preview__info__title">{degree} {course}</h4>
+      <p>{school}</p>
+      <p className="preview__info__date">{formatDate(graduationYear)}</p>
     </div>
   );
 };
--- src/components/WorkPreview.js
+++ src/components/WorkPreview.js
@@ -5,16 +5,14 @@ const WorkPreview = (props) => {
 
   return (
     <div className="preview__info">
-      <div>
-        <h4 className="preview__info__title">{position}</h4>
-        <p>{organisation}</p>
-        {startDate && (
-          <p className="preview__info__date">
-            <span>{formatDate(startDate)}</span> -{' '}
-            <span>{endDate ? formatDate(endDate) : 'Current'}</span>
-          </p>
-        )}
-      </div>
+      <h4 className="preview__info__title">{position}</h4>
+      <p>{organisation}</p>
+      {startDate && (
+        <p className="preview__info__date">
+          <span>{formatDate(startDate)}</span> -{' '}
+          <span>{endDate ? formatDate(endDate) : 'Current'}</span>
+        </p>
+      )}
       {description && <p className="preview__info_description">{description}</p>}
     </div>
   );

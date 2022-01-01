ae7dc3d1d21e453c0d84b797e2251d383c6d801f
Peter Abah <peterabah.ob@gmail.com>
2022-01-01 18:36:03 +0100

    Change Preview compoment to be functional

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,73 +1,71 @@
-import { Component } from 'react';
+import React from 'react';
 import WorkPreview from './WorkPreview';
 import EducationPreview from './EducationPreview';
 
-class Preview extends Component {
-  render() {
-    const { personal, work, education, skills } = this.props;
-    const { name, email, address, phone, description } = personal.entry;
+const Preview = React.forwardRef((props, ref) => {
+  const { personal, work, education, skills } = props;
+  const { name, email, address, phone, description } = personal.entry;
 
-    return (
-      <div className="preview">
-        <div className="preview__main">
-          <div className="preview__section">
-            <h2 className="preview__name">{name}</h2>
-            <p className="preview__description">{description}</p>
-          </div>
+  return (
+    <div ref={ref} className="preview">
+      <div className="preview__main">
+        <div className="preview__section">
+          <h2 className="preview__name">{name}</h2>
+          <p className="preview__description">{description}</p>
+        </div>
 
-          <div className="preview__section">
-            <h2 className="preview__section__title">Work Experience</h2>
-            {work.list.map((entry) => (
-              <WorkPreview key={entry.id} {...entry} />
-            ))}
-          </div>
+        <div className="preview__section">
+          <h2 className="preview__section__title">Work Experience</h2>
+          {work.list.map((entry) => (
+            <WorkPreview key={entry.id} {...entry} />
+          ))}
+        </div>
 
-          <div className="preview__section">
-            <h2 className="preview__section__title">Education</h2>
-            {education.list.map((entry) => (
-              <EducationPreview key={entry.id} {...entry} />
-            ))}
+        <div className="preview__section">
+          <h2 className="preview__section__title">Education</h2>
+          {education.list.map((entry) => (
+            <EducationPreview key={entry.id} {...entry} />
+          ))}
+        </div>
+      </div>
+
+      <aside className="preview-sidebar">
+        <div className="preview-sidebar__section">
+          <h2 className="preview-sidebar__section__title">Contact details</h2>
+          <div className="preview-sidebar__section__details">
+            <div className="preview-sidebar__info">
+              <h3 className="preview-sidebar__info__title">EMAIL</h3>
+              <p>{email}</p>
+            </div>
+
+            <div className="preview-sidebar__info">
+              <h3 className="preview-sidebar__info__title">ADDRESS</h3>
+              <p>{address}</p>
+            </div>
+
+            <div className="preview-sidebar__info">
+              <h3 className="preview-sidebar__info__title">PHONE</h3>
+              <p>{phone}</p>
+            </div>
           </div>
         </div>
 
-        <aside className="preview-sidebar">
-          <div className="preview-sidebar__section">
-            <h2 className="preview-sidebar__section__title">Contact details</h2>
-            <div className="preview-sidebar__section__details">
-              <div className="preview-sidebar__info">
-                <h3 className="preview-sidebar__info__title">EMAIL</h3>
-                <p>{email}</p>
-              </div>
-
-              <div className="preview-sidebar__info">
-                <h3 className="preview-sidebar__info__title">ADDRESS</h3>
-                <p>{address}</p>
-              </div>
-
-              <div className="preview-sidebar__info">
-                <h3 className="preview-sidebar__info__title">PHONE</h3>
-                <p>{phone}</p>
-              </div>
-            </div>
+        <div className="preview-sidebar__section">
+          <h2 className="preview-sidebar__section__title">Skills</h2>
+          <div className="preview-sidebar__section__details">
+            {skills.list.map((entry) => (
+              <p
+                className="preview-sidebar__info preview-sidebar__info__title"
+                key={entry.id}
+              >
+                {entry.name}
+              </p>
+            ))}
           </div>
-
-          <div className="preview-sidebar__section">
-            <h2 className="preview-sidebar__section__title">Skills</h2>
-            <div className="preview-sidebar__section__details">
-              {skills.list.map((entry) => (
-                <p
-                  className="preview-sidebar__info preview-sidebar__info__title"
-                  key={entry.id}
-                >
-                  {entry.name}
-                </p>
-              ))}
-            </div>
-          </div>
-        </aside>
-      </div>
-    );
-  }
-}
+        </div>
+      </aside>
+    </div>
+  );
+});
 
 export default Preview;
